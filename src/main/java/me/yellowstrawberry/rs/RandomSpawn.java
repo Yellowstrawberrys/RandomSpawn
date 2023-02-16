fe245fbe66b79de5f3e7507142a780c8d18a5823
Yellowstrawberrys <dirjhhsg@gmail.com>
2023-02-17 02:05:51 +0900

    Release 0.0.1

---
+++ src/main/java/me/yellowstrawberry/rs/RandomSpawn.java
@@ -0,0 +1,107 @@
+package me.yellowstrawberry.rs;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Random;
+
+public final class RandomSpawn extends JavaPlugin implements Listener {
+
+    HashMap<String, Location> spawnpoints = new HashMap<>();
+    Connection conn;
+
+    Settings settings = new Settings(Bukkit.getPluginsFolder());
+
+    @Override
+    public void onEnable() {
+        try {
+            if(settings.sqliteFile == null) {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conn = DriverManager.getConnection(
+                        "jdbc:mariadb://" + settings.sqlURL + "/" + settings.databaseName + "?useUnicode=true&passwordCharacterEncoding=utf-8", "root", "root");
+                conn.prepareStatement("CREATE TABLE IF NOT EXISTS `spawnpoint` (\n" +
+                        "\t`uuid` LONGTEXT NOT NULL COLLATE 'utf8mb4_general_ci',\n" +
+                        "\t`x` INT(11) NOT NULL DEFAULT '0',\n" +
+                        "\t`y` INT(11) NOT NULL DEFAULT '0',\n" +
+                        "\t`z` INT(11) NOT NULL DEFAULT '0'\n" +
+                        ")").execute();
+            }else {
+                Class.forName("org.sqlite.JDBC");
+                conn = DriverManager.getConnection("jdbc:sqlite:"+settings.sqliteFile.getAbsolutePath());
+            }
+        } catch (Exception se) {
+            se.printStackTrace();
+        }
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(!Bukkit.getOfflinePlayer(event.getPlayer().getUniqueId()).hasPlayedBefore()) {
+            Location loc = getRandomCoordinates();
+            addSpawnpoint(event.getPlayer().getUniqueId().toString(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+            event.getPlayer().setBedSpawnLocation(loc);
+            event.getPlayer().setCompassTarget(loc);
+            event.getPlayer().teleport(loc);
+        }
+    }
+
+    public Location getRandomCoordinates() {
+        Random r = new Random();
+        int x = settings.centerX+(r.nextInt(settings.maxX*2)-settings.maxX), z = settings.centerZ+(r.nextInt(settings.maxZ*2)-settings.maxZ);
+        int y = Bukkit.getWorld("world").getHighestBlockYAt(x, z);
+        Location loc = new Location(Bukkit.getWorld("world"), x, y, z);
+        Bukkit.getWorld("world").loadChunk(loc.getChunk());
+        if(loc.getBlock().getType() == Material.WATER || loc.getBlock().getType() == Material.LAVA) loc = getRandomCoordinates();
+        return loc;
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        if(event.getPlayer().getBedSpawnLocation() == null) {
+            event.getPlayer().setBedSpawnLocation(getSpawnpoint(event.getPlayer().getUniqueId().toString()));
+            event.setRespawnLocation(getSpawnpoint(event.getPlayer().getUniqueId().toString()));
+        }
+    }
+
+    public void addSpawnpoint(String uuid, int x, int y, int z){
+        try {
+            spawnpoints.put(uuid, new Location(Bukkit.getWorld("world"), x, y, z));
+            conn.prepareStatement("INSERT INTO `spawnpoint` (uuid, x, y, z) VALUES ('"+uuid+"', "+x+", "+y+", "+z+")").execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Location getSpawnpoint(String uuid) {
+        if(spawnpoints.containsKey(uuid)) return spawnpoints.get(uuid);
+        try{
+            ResultSet set = conn.prepareStatement("SELECT * FROM `spawnpoint` WHERE uuid='"+uuid+"'").executeQuery();
+            set.first();
+            spawnpoints.put(uuid, new Location(Bukkit.getWorld("world"), set.getInt("x"), set.getInt("y"), set.getInt("z")));
+            return spawnpoints.get(uuid);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/me/yellowstrawberry/rs/Settings.java
@@ -0,0 +1,81 @@
+package me.yellowstrawberry.rs;
+
+import com.google.common.io.Files;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.Map;
+
+public class Settings {
+
+    Yaml yaml = new Yaml();
+    File root;
+    File sqliteFile;
+
+    int centerX = 0;
+    int centerZ = 0;
+    int maxX = 29999984;
+    int maxZ = 29999984;
+
+    String sqlURL = "localhost:3306";
+    String sqlUser = "root";
+    String sqlPassword = "root";
+    String databaseName = "server";
+
+    boolean canSpawnInWater = false;
+
+    public Settings(File pluginFolder) {
+        root = new File(pluginFolder+"/RandomSpawn/");
+        if(!root.exists()) root.mkdirs();
+
+        try {
+            loadSettings();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadSettings() throws IOException {
+        File settingFile = new File(root+"/settings.yaml");
+        if(!settingFile.exists()) {
+            copySample(Settings.class.getResourceAsStream("/sample.yaml"), settingFile);
+            sqliteFile = new File(root+"/server.db");
+            if(!sqliteFile.exists()) copySample(Settings.class.getResourceAsStream("/sample.db"), sqliteFile);
+        }else {
+            Map<String, Object> settings = yaml.load(new FileInputStream(settingFile));
+
+            Map<String, Object> coordinateS = ((Map<String, Object>) settings.get("coordinates"));
+            Map<String, Object> sqlS = ((Map<String, Object>) settings.get("sql"));
+
+            if(coordinateS.get("type").toString().equals("custom")) {
+                if(coordinateS.containsKey("centerX")) centerX = (Integer) coordinateS.get("centerX");
+                if(coordinateS.containsKey("centerZ")) centerZ = (Integer) coordinateS.get("centerZ");
+                if(coordinateS.containsKey("maxX")) maxX = (Integer) coordinateS.get("maxX");
+                if(coordinateS.containsKey("maxZ")) maxZ = (Integer) coordinateS.get("maxZ");
+                if(coordinateS.containsKey("canSpawnInWater")) canSpawnInWater = ((String) coordinateS.get("canSpawnInWater")).equalsIgnoreCase("true");
+            }
+
+            if(sqlS.get("type").toString().equals("sqlite")) {
+                sqliteFile = new File(root+"/server.db");
+                if(!sqliteFile.exists()) copySample(Settings.class.getResourceAsStream("/sample.db"), sqliteFile);
+            }else if(sqlS.get("type").toString().equals("mariadb")) {
+                sqliteFile = null;
+
+                if(coordinateS.containsKey("databaseName")) databaseName = coordinateS.get("databaseName").toString();
+                if(coordinateS.containsKey("url")) sqlURL = coordinateS.get("url").toString();
+                if(coordinateS.containsKey("user")) sqlUser = coordinateS.get("user").toString();
+                if(coordinateS.containsKey("password")) sqlPassword = coordinateS.get("password").toString();
+            }
+        }
+    }
+
+    public void copySample(InputStream ipt, File file) throws IOException {
+        FileOutputStream opt = new FileOutputStream(file);
+
+        byte[] b = new byte[1024];
+
+        while (ipt.read(b) != -1) {
+            opt.write(b);
+        }
+    }
+}
